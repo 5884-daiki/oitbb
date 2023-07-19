0020154bdd831c24dd65fd90ee83cff4ccb083bd
daiki <e1x21040@oit.ac.jp>
2023-07-19 15:34:43 +0900

    テンプレt－と場所

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -16,3 +16,24 @@ class PostAdmin(admin.ModelAdmin):
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
     
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+
+# 変更履歴を取得したいモデルのIDを指定
+model_id = 1  # ここではモデルのIDを1に仮定しています
+
+# ログエントリをフィルタリング
+log_entries = LogEntry.objects.filter(object_id=model_id)
+
+# 各ログエントリをループして表示
+for entry in log_entries:
+    # ログのタイプを取得して表示
+    if entry.action_flag == ADDITION:
+        action = "追加"
+    elif entry.action_flag == CHANGE:
+        action = "変更"
+    elif entry.action_flag == DELETION:
+        action = "削除"
+    else:
+        action = "不明"
+
+    print(f"ID: {entry.id}, ユーザー: {entry.user}, アクション: {action}, 時間: {entry.action_time}")
